f466e538b6f70865b456d107c5353753d3a8ff8a
tunomizu <tumikihurai46@gmail.com>
2016-11-04 11:32:10 +0900

    統合

---
+++ Assets/Nakagawa/Pausable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+
+
+public class Pausable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Nakagawa/Script/mainCamera1.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainCamera1 : MonoBehaviour {
+    public Transform target;
+
+    private Vector3 offset;
+
+    // public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        // offset = transform.position - target.transform.position;
+
+        target = GameObject.Find("Player").transform;
+
+    }
+
+    void Update()
+    {
+        // transform.position = target.position + offset;
+
+        transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+
+
+        /* if (transform.position.x >= 30)
+         {
+             transform.position = new Vector3(18, transform.position.y, transform.position.z);
+         }*/
+
+    }
+
+}
